c2bc8c3ff63a320e57fdb59a16a03db6081f9d35
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-09-29 19:31:01 +0300

    While the test highlights that the new array is not defined, its giving the required outcome

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+//task1: Returns a new array with a display message to the members of the array
+const members = ["Guadalupe", "Ollie", "Aki"]
+
+const newCards = members.map(function writeCards(members, surprise){
+   
+    for(let i= 0; i < members.length; i++){
+      // console.log ( `Thank you, ${members[i]}, for the wonderful ${event} gift!`);
+    
+    return`Thank you, ${members}, for the wonderful ${"surprise"} gift`;
+    
+    }
+}
+)
+
+console.log(newCards);
+
+//task 2: 
+function countDown(){
+let number= 10;
+while( number >= 0){
+    console.log(number--);
+}
+}
+
+  
+
+console.log(countDown());
+ 
